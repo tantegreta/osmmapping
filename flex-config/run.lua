caa5650ff1321081d2753425880565e773a69e3f
tantegreta <111191257+tantegreta@users.noreply.github.com>
2022-08-14 16:15:52 +0200

    Create run.lua

---
+++ flex-config/run.lua
@@ -0,0 +1,7 @@
+-- Loads the `conf` var from layerset INI file
+
+-- Load the layerset configuration (e.g. PGOSM_LAYERSET, PGOSM_LAYERSET_PATH" etc.)
+require "layerset"
+
+-- Git settings, import meta data etc.
+require "style.pgosm-meta"
